513a0c86bd862e0ba4223cf74572076638f928c8
teju9805 <167278047+teju9805@users.noreply.github.com>
2024-04-29 10:58:18 +0530

    Add files via upload

---
+++ commands.ps1
@@ -0,0 +1,13 @@
+﻿md parent
+md parent/c1
+md parent/c2
+md parent/c3
+md parent/c4
+echo "welcome to python proramming" > parent/c1/c1t1.txt
+echo "welcome to python proramming" > parent/c2/c2t2.txt
+echo "welcome to python proramming" > parent/c3/c3t3.txt
+echo "welcome to python proramming" > parent/c4/c4t4.txt
+type parent/c1/c1t1.txt
+type parent/c2/c2t2.txt
+type parent/c3/c3t3.txt
+type parent/c4/c4t4.txt
